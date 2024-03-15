2c0e28cedfa95522420ff91dc58cb37dc5c9edb4
youngmanpark <py980627@gmail.com>
2024-03-15 13:43:09 +0900

    new commit

--- 6_직사각형에서 탈출.py
+++ 6_직사각형에서 탈출.py
@@ -1,1 +1,7 @@
-#1085
+# 1085
+x, y, w, h = input().split()
+
+list = []
+list=[int(x), int(y), abs(int(x) - int(w)), abs(int(y) - int(h))]
+list.sort()
+print(list[0])
---
+++ 7_구구단.py
@@ -0,0 +1,4 @@
+#2739
+x = int(input())
+for i in range(1,10,1):
+    print(x ,'*',i,'=',x*i)
