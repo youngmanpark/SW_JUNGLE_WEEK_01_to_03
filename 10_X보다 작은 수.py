1a427f8985dd2af6d5651bc6ee9a8f7cbc090e9b
youngmanpark <py980627@gmail.com>
2024-03-15 14:11:37 +0900

    new commit

---
+++ 10_X보다 작은 수.py
@@ -0,0 +1,7 @@
+# 10871
+n, x = input().split()
+list = []
+k = input().split()
+for i in k:
+    if (int(i) < int(x)):
+        print(i,end=" ")
---
+++ 8_A+B-3.py
@@ -0,0 +1,6 @@
+#10950
+n = int(input())
+
+for i in range(n):
+    a,b=input().split()
+    print(int(a)+int(b))
