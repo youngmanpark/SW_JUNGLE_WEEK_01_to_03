43be6b5e537ef552317e2d85a1d621579f82d75d
youngmanpark <py980627@skuniv.ac.kr>
2024-03-24 14:53:17 +0900

    new commit

--- WEEK02/우선순위 큐/17_철로.py
+++ WEEK02/우선순위 큐/17_철로.py
@@ -7,7 +7,7 @@ input = sys.stdin.readline
 n = int(input())
 office = []
 
-# n명의 사람들의 사무실과 집위치를 (가까운곳-먼곳)순으로 큐에 담아준다
+# n명의 사람들의 사무실과 집위치를 (가까운곳-먼곳순 정렬) 큐에 담아준다 최종적으로는 먼곳이 앞으로 오도록
 for _ in range(n):
     start, end = map(int, input().split())
     if start > end:
@@ -23,7 +23,7 @@ d = int(input())
 possible = []
 res = 0
 cnt = 0
-while office:
+while office: #사람들의 위치 정보를 다 빼냈을 때
     end, start = heapq.heappop(office)
     if start >= end - d:
         heapq.heappush(possible, (start, end))
