711fb98718faac842a725470d5b19e51a99573cd
youngmanpark <py980627@gmail.com>
2024-03-29 17:36:11 +0900

    new commit

--- WEEK03/그리디/21_잃어버린 괄호.py
+++ WEEK03/그리디/21_잃어버린 괄호.py
@@ -1,2 +1,19 @@
-#1541
+# 1541
 
+import sys
+
+input = sys.stdin.readline
+m = input().strip().split('-')
+
+tmp = []
+for i in m:
+    cnt = 0
+    for j in i.split('+'):
+        cnt += int(j)
+    tmp.append(cnt)
+
+result = tmp[0]
+for i in tmp[1:]:
+    result -= i
+
+print(result)
