1dd22eaee4b0ef2d3da0bb698a1d8f73d3bb7678
youngmanpark <py980627@gmail.com>
2024-03-29 09:50:15 +0900

    new commit

---
+++ WEEK03/동적프로그래밍/01_피보나치 수 2.py
@@ -0,0 +1,13 @@
+# 2748
+# 디피는 기본적으로 배열을 잘 활용해야 하는듯?
+# 배열에 넣어두고 꺼내면서 확인?
+n=int(input())
+
+fibo=[]
+fibo.append(0)
+fibo.append(1)
+
+for i in range(2,n+1):
+    fibo.append(fibo[i-1]+fibo[i-2])
+
+print(fibo[n])
