8f305139aad07dd8ee0a9c20c6d2a999b50b5e6c
youngmanpark <py980627@gmail.com>
2024-03-30 22:18:21 +0900

    new commit

---
+++ WEEK03/동적프로그래밍/05_동전.py
@@ -0,0 +1,22 @@
+# 9084
+import sys
+
+input = sys.stdin.readline
+T = int(input())
+for t in range(T):
+    n = int(input())  # 동전 가지수
+    coins = list(map(int, input().split()))  # 동전 금액 집합
+    money = int(input())  # 금액
+
+    dp = [[0 for _ in range(money + 1)] for _ in range(n + 1)]
+
+    dp[0][0] = 1
+
+    for c in range(1, len(coins) + 1):  # 코인부터
+        for m in range(money + 1):  # 머니머니~
+            dp[c][m] = dp[c - 1][m]
+        for m in range(1, money + 1):
+            if m - coins[c - 1] >= 0:
+                dp[c][m] += dp[c][m - coins[c - 1]]
+
+    print(dp[n][money])
